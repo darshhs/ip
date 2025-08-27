790e6ddcca40dea5a9b2bf8cdb97e12a86b7ddb7
darshhs <darshanasur1204@gmail.com>
2025-08-27 23:54:54 +0800

    updated Michael with echo

--- src/main/java/Michael.java
+++ src/main/java/Michael.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Michael {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,7 +9,20 @@ public class Michael {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String welcome = "Hello! I'm Michael\n" +
                          "What can I do for you?";
+
+        System.out.println(welcome);
+
+        Scanner in = new Scanner(System.in);
+        String line = "";
+
+        String ending_line =  "bye";
         String exit = "Bye. Hope to see you again soon!";
-        System.out.println(welcome + "\n\n" + exit);
+
+        while (!(line.equals(ending_line))) {
+            line = in.nextLine();
+            System.out.println(line);
+        }
+        System.out.println(exit);
+
     }
 }
