51da06048d2021ec15f8654a25dcb52d5fdde5c2
darshhs <darshanasur1204@gmail.com>
2025-08-27 23:30:57 +0800

    changed name of bot to Michael

--- src/main/java/Michael.java
+++ src/main/java/Michael.java
@@ -1,10 +1,13 @@
-public class Duke {
+public class Michael {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcome = "Hello! I'm Michael\n" +
+                         "What can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(welcome + "\n\n" + exit);
     }
 }
