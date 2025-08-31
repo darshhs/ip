56ee2329a46a661cd159552febda883b0ccc2d41
darshhs <darshanasur1204@gmail.com>
2025-08-31 19:46:30 +0800

    Added getStatusIcon to main

--- src/main/java/Michael.java
+++ src/main/java/Michael.java
@@ -16,7 +16,7 @@ public class Michael {
         String line = "";
 
         String ending_line = "bye";
-        String exit = "Bye. Hope to see you again soon!";
+        String exit = "Bye! Hope to see you again soon!";
 
 
         while (!(line.equals(ending_line))) {
@@ -33,21 +33,21 @@ public class Michael {
             }
 
             if (line.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
+                System.out.println("Hello there! Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println((i + 1) + "." + "[" + ((tasks.get(i).isDone) ? "X" : " ") + "] " + tasks.get(i).description);
                 }
             } else if (Instruction[0].equals("mark")) {
                 if (index >= 0 && index < tasks.size()) {
                     tasks.get(index).markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  [" + ((tasks.get(index).isDone) ? "X" : " ") + "] " + tasks.get(index).description);
+                    System.out.println("Good job! I've marked this task as done:");
+                    System.out.println("  [" + tasks.get(index).getStatusIcon() + "] " + tasks.get(index).description);
                 }
             } else if (Instruction[0].equals("unmark")) {
                 if (index >= 0 && index < tasks.size()) {
                     tasks.get(index).markAsUndone();
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("  [" + ((tasks.get(index).isDone) ? "X" : " ") + "] " + tasks.get(index).description);
+                    System.out.println("Alright, I've marked this task as not done yet:");
+                    System.out.println("  [" + tasks.get(index).getStatusIcon() + "] " + tasks.get(index).description);
                 }
             } else {
                 if (!line.equals(ending_line)) {
